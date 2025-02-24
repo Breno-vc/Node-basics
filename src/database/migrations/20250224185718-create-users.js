19759fe29bffe7c8c1781123e3c881f72f2c793d
Breno Carvalho <carv.breno.dev@gmail.com>
2025-02-24 16:04:55 -0300

    feat: add migrations to the project

---
+++ src/database/migrations/20250224185718-create-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: { type: Sequelize.STRING, allowNull: false },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    provider: { type: Sequelize.BOOLEAN, allowNull: false, default: false },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  });
+}
+
+export async function down(queryInterface) {
+  await queryInterface.dropTable('users');
+}
